efb519508c1d4773efd74a5cb02e3b4fe7ec770e
Hospiea <155417815+Hospiea@users.noreply.github.com>
2025-11-22 06:30:36 +0900

    feat: update CORS configuration to allow requests from https://hdev.pics

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ["http://localhost:3000"]
+    origin: ["http://localhost:3000", "https://hdev.pics"]
   })
   await app.listen(process.env.PORT ?? 5050);
 }
